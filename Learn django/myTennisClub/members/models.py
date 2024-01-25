a01bcddfdb3013ca956b4c2a669877281e29c03b
Zaheer Ahmad <zaheerahmadlangrial92@gamil.com>
2024-01-25 13:30:01 +0500

    Learn Django

--- Learn django/myTennisClub/members/models.py
+++ Learn django/myTennisClub/members/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Members (models.Model) : 
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
+    phone = models.IntegerField(null=True) 
+    joined_date = models.DateField(null=True) 
+
+    def __str__(self) : 
+        return f"{self.firstName} {self.lastName}"
